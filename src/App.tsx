96d7b941eacd0c085420e81a368eb1b228db8cdd
andreyshalomitsky <andreyshalomitsky@gmail.com>
2024-05-06 10:38:37 +0500

    change Typography + Typography stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import { TextField } from '@/copmponents/ui/text-field'
 
+import { Typography } from '@/copmponents/ui/typography'
+
 export function App() {
   return (
     <div>
       Hello
       {/*<TextField>Eeee</TextField>*/}
+      <Typography>A</Typography>
     </div>
   )
 }
